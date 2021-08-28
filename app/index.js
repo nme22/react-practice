8225154856f75e67fbb09667e04820b0824d7197
Navid Ebrahimi <navidmebrahimi22@gmail.com>
2021-08-28 17:36:46 -0400

    popular.js

--- app/index.js
+++ app/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Badge from './badge';
-import Hello from './Hello';
+import Badge from './components/badge';
+import Hello from './components/Hello';
+import Popular from './components/popular';
 import './index.css';
 
 //Component
@@ -15,18 +16,15 @@ import './index.css';
 class App extends React.Component{
     render() {
         return (
-        <div> Hello Navid! Congrats on creating your first react component! </div>
+        <div className='container'> 
+            <Popular/>
+        </div>
         )
     }
 };
 
 ReactDOM.render(
-    <Badge user={{
-        name: 'Navid Ebrahimi',
-        img: 'https://scontent.fewr1-6.fna.fbcdn.net/v/t1.6435-9/142276688_4014998048520117_5998950829911969368_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=0bXUq48HulsAX8ztuMZ&_nc_ht=scontent.fewr1-6.fna&oh=e2b1d6450283c37ac82d80a677696319&oe=614FF81F',
-        username: 'nme22'
-    }}
-    />,
-    //Where do we want to render this element to?
+    <App/>,
+    
     document.getElementById('App')
 );
