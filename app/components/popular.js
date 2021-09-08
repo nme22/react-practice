f1cd779f073025fdd34089797ee3e6a404f31d37
Navid Ebrahimi <navidmebrahimi22@gmail.com>
2021-09-08 12:15:00 -0400

    user,stars,forks,issues

--- app/components/popular.js
+++ app/components/popular.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
 import { fetchPopularRepos } from './utils/api'
+import {FaUser, FaStar, FaCodeBranch, FaExclamationTriangle} from 'react-icons/fa'
 
 function LangaugesNav ({ selected, onUpdateLanguage }) {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
@@ -42,6 +43,30 @@ function ReposGrid ({ repos }) {
                         <h2 className='center-text'>
                             <a className='link' href={html_url}> {login} </a>
                         </h2>
+                        <ul className='card-list'>
+                            <li>
+                                <FaUser color='rgb(255,191,116)' size={22}/>
+                                <a href={`https://github.com/${login}`}>
+                                    {login}
+
+                                </a>
+                            </li>
+
+                            <li>
+                                <FaStar color='rgb(255,215,0)' size={22}/>
+                                {stargazers_count.toLocalString()} stars
+                            </li>
+
+                             <li>
+                             <FaCodeBranch color='rgb(129,195,245)' size={22}/>
+                                {forks.toLocalString()} forks
+                            </li>
+
+                            <li>
+                            <FaExclamationTriangle color='rgb(241,138,147)' size={22}/>
+                                {open_issues.toLocalString()} open_issues
+                            </li>
+                        </ul>
 
                     </li>
                 )
