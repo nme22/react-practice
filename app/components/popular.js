eb234a69623024c204de0a8c93a376bc08b1dda8
Navid Ebrahimi <navidmebrahimi22@gmail.com>
2021-09-13 23:40:51 -0400

    got the results array working

--- app/components/popular.js
+++ app/components/popular.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
-import { fetchPopularRepos } from './utils/api'
+import { fetchPopularRepos } from '../utils/api'
 import {FaUser, FaStar, FaCodeBranch, FaExclamationTriangle} from 'react-icons/fa'
 
 function LangaugesNav ({ selected, onUpdateLanguage }) {
