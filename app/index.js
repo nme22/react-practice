8c510b3ceb944408a18e930d054e3fff5dd8e8b5
Navid Ebrahimi <navidmebrahimi22@gmail.com>
2021-09-16 00:12:53 -0400

    refactor similar code

--- app/index.js
+++ app/index.js
@@ -16,7 +16,7 @@ class App extends React.Component{
     render() {
         return (
         <div className='container'> 
-            <Popular/>
+            <Battle/>
         </div>
         )
     }
