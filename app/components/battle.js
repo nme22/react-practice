46a301f0a5160da653482d9fcd8880ac2389dad5
Navid Ebrahimi <navidmebrahimi22@gmail.com>
2021-10-05 19:07:37 -0400

    fixing up battle.js

--- app/components/battle.js
+++ app/components/battle.js
@@ -140,17 +140,17 @@ export default class Battle extends React.Component {
     const { playerOne, playerTwo, battle } = this.state
 
     if (battle === true) {
-        return <Results
-         playerOne={playerOne}
-        playerTwo={playerTwo}
-        onReset={() => this.setState({
+      return (
+        <Results
+          playerOne={playerOne}
+          playerTwo={playerTwo}
+          onReset={() => this.setState({
             playerOne: null,
             playerTwo: null,
             battle: false
-        })}
-        
-        
+          })}
         />
+      )
     }
 
     return (
@@ -185,15 +185,15 @@ export default class Battle extends React.Component {
             }
           </div>
 
-            {playerOne && playerTwo && (
-                <button
-                className='btn dark-btn btn-space'
-                onClick={() => this.setState({battle: true})}
-                >
-                    Battle
-                </button>
-            )}
 
+          {playerOne && playerTwo && (
+            <button
+              className='btn dark-btn btn-space'
+              onClick={() => this.setState({battle: true})}
+            >
+              Battle
+            </button>
+          )}
         </div>
       </React.Fragment>
     )
